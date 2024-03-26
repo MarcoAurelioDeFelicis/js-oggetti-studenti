5ca72f35d1f5c92d8f0e56af1c96e018a98dd92b
MarcoAurelioDeFelicis <marcodefelicis24@gmail.com>
2024-03-26 17:33:11 +0100

    ADD: array oggetti studenti

--- js/script.js
+++ js/script.js
@@ -1,3 +1,30 @@
 console.log ("Oggetti Studenti");
 
+const Studenti = [
+    {
+        nome: 'Marco',
+        cognome: 'De felicis',
+        età: '22',
+    },
+    {
+        nome: 'Fabio',
+        cognome: 'Moro',
+        età: '25',
+    },
+    {
+        nome: 'Leonardo',
+        cognome: 'Leo',
+        età: '21',
+    },
+    {
+        nome: 'Francesca',
+        cognome: 'Russo',
+        età: '24',
+    },
+    {
+        nome: 'Sara',
+        cognome: 'Celeste',
+        età: '20',
+    },
+]
 
